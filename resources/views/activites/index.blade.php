364f065764d75a8809893778908ad7cfd8620ef9
fahikatbakary-lab <fahikatbakary@gmail.com>
2026-06-01 21:19:22 +0200

    la plateforme est parfaite

--- resources/views/activites/index.blade.php
+++ resources/views/activites/index.blade.php
@@ -149,17 +149,7 @@
                                 </svg>
                             </a>
 
-                            {{-- Modifier --}}
-                            @if($activite->statut === 'en_attente')
-                            <a href="{{ route('activites.edit', $activite) }}"
-                               class="btn btn-sm btn-icon btn-outline-primary" title="Modifier">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                     stroke="currentColor" style="width:14px;height:14px">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                          d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"/>
-                                </svg>
-                            </a>
-                            @endif
+                            {{-- La modification d'activité n'est pas disponible à ce stade --}}
 
                             @role('admin|secretaire')
                             @if($activite->statut === 'en_attente')
